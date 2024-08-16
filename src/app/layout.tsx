f4ea263424918ada9662ddbf290fbe63f8edbd2e
Adebayour66265 <adebayour66265@gmail.com>
2024-08-16 22:36:11 +0100

    add snow effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
+import Script from "next/script"; // Import Script from next/script
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -19,6 +20,13 @@ export default function RootLayout({
     <html lang="en" className="relative">
       <body className={cn(dmSans.className, "antialiased bg-[#fff]")}>
         {children}
+
+        {/* JavaScript code for snow effect*/}
+        <Script src="/snowEffect.js" defer>
+          {`
+            console.log('JavaScript code running in RootLayout');
+          `}
+        </Script>
       </body>
     </html>
   );
